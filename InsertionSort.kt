2c0df6cc8d0dd8c90659b476fab1615234c30cc1
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-04-08 11:31:19 -0700

    Update InsertionSort.kt

--- InsertionSort.kt
+++ InsertionSort.kt
@@ -1,12 +1,13 @@
 @ComparisionSort
 @StableSort
 
+
 fun main(){
-  val arr = [3,4,2,1];
+  var arr = arrayOf(3,4,2,1,555,1,0,-8);
   for( i in 1 until arr.size){
     for(j in i downTo 1){
       if(arr[j-1] < arr[j])break
-      else arr.exch(j,j-1)
+      else arr[j-1] = arr[j].also({arr[j]=arr[j-1]})
       // we are simply transversing to each index and putting it to its right pos 
       // in already left sorted array
       // start from [3] sorted [4,2,1] unsorted
@@ -14,5 +15,9 @@ fun main(){
       // then [2,3,4] [1]
       // then [1,2,3,4]
       
+     }
   }
+
+   for(x in arr)print(x.toString() + " ");
+  
 }
